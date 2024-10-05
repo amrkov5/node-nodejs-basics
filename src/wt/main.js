383245913c56830525e4cf64a71d9ab86cbb42ac
Anton markov <amrkov5@gmail.com>
2024-10-06 00:19:49 +0300

    tasks solved

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { cpus } from 'os';
+import path from 'path';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const pathToWorker = path.join(import.meta.dirname, 'worker.js');
+  const cpu = cpus().length;
+  const promises = [];
+  for (let i = 0; i < cpu; i += 1) {
+    const data = 10 + i;
+    const promise = new Promise((resolve) => {
+      const worker = new Worker(pathToWorker, { workerData: data });
+      worker.on('message', (result) => {
+        resolve({ status: 'resolved', data: result });
+      });
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null });
+      });
+    });
+    promises.push(promise);
+  }
+  Promise.all(promises).then((results) => console.log(results));
 };
 
 await performCalculations();
